6e545c37ae32d931940213e449ed7b331d860aea
ezekie97 <ezekie97@users.noreply.github.com>
2014-02-14 13:34:54 -0500

    Driver (New Setup)
    Driver Class for the new Setup

--- Driver.java
+++ Driver.java
@@ -1,17 +1,18 @@
+
 /**
  * Write a description of class Driver here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
- 
 public class Driver
 {
     private Solver solver = new Solver();
 
     public Driver()
     {
-        solver.readFile("test1.txt",0);
+        solver.readFile("test1.txt"); //f f f f
+        solver.readFile("s28.cnf");
     }
 
 }
