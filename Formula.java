327cb1f3aa6d5c4ad025d9844b5a57c9d3768320
ezekie97 <ezekie97@users.noreply.github.com>
2014-02-17 20:07:44 -0500

    Update Formula.java

--- Formula.java
+++ Formula.java
@@ -1,32 +1,43 @@
 import java.util.ArrayList;
 /**
- * A Clause is a disjunction of boolean variables.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * A Formula is a list of clauses.
+ * If one clause is false, the whole formula is false.
+ * @author Bill Ezekiel, Matthew Martorana, James O'Donnell 
+ * @version (2/17/14)
  */
 public class Formula
 {
     private ArrayList<Clause> clauses;
-    
+    /**
+     * Create a formula with 'n' clauses.
+     * @param n the number of clauses in this formula.
+     */
     public Formula(int n)
     {
         clauses = new ArrayList(n);
     }
 
     /**
-     * Evaluate this clause
+     * Evaluate this formula using a collection of boolean values.
+     * @param b An ArrayList containing boolean values.
+     * @return true only if this formula evaluates to true.
      */
     public boolean satisfies(ArrayList<Boolean> b)
     {
         for(Clause c: clauses)
         {
+            //since a formula is a conjuction, if one clause returns
+            //false, the whole formula is false.
             if(!(c.satisfies(b)))
                 return false;       
         }
         return true;
     }
 
+    /**
+     * Add a clause to this formula
+     * @param c the clause to be added.
+     */
     public void addClause(Clause c)
     {
         clauses.add(c);
