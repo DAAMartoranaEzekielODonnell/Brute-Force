f69f9028b102069b0e903fe439e1a0a68ffaef44
ezekie97 <ezekie97@users.noreply.github.com>
2014-02-17 20:08:36 -0500

    Update Variable.java

--- Variable.java
+++ Variable.java
@@ -2,28 +2,39 @@
 /**
  * A Variable that has a number
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Bill Ezekiel, Matthew Martorana, James O'Donnell  
+ * @version (2/17/14)
  */
 public class Variable
 {
-    private int number;
+    private int number; //variable number (x#)
+    private boolean not = false;
     /**
      * Constructor for a Variable
+     * @param i The variable number
      */
     public Variable(int i)
     {
-        number = i;
+        if(i<0)
+        {
+            not = true;  //not
+            number= -i; //make positive
+        }
+        else{
+            number = i;
+        }
     }
 
     /**
-     * @param b  A boolean 
+     * Evaluate a boolean value for this variable.
+     * @param b a boolean value (true or false)
+     * @return a boolean value
      */
     public boolean getValue(boolean b)
     {
-        if(number>0)
-        {return b;}
-        return !b;
+        if(not)  
+        {return !b;}
+        return b;
     }
 
     /**
