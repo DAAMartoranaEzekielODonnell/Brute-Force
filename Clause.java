f9749d4bdb2c69727b925e76dc1d1ab923a492fd
ezekie97 <ezekie97@users.noreply.github.com>
2014-02-17 20:08:04 -0500

    Update Clause.java

--- Clause.java
+++ Clause.java
@@ -1,28 +1,38 @@
 import java.util.ArrayList;
 /**
- * A Clause is a disjunction of boolean variables.
+ * A Clause is a disjunction of boolean Variables.
+ * If one variable is true, the whole clause is true.
  * 
- * @author //Names here 
- * @version //date here
+ * @author Bill Ezekiel, Matthew Martorana, James O'Donnell 
+ * @version (2/17/14)
  */
 public class Clause
 {
     private ArrayList<Variable> variables;
-    
+
+    /**
+     * Create a clause with 'n' variables.
+     * @param n the number of variables in this clause.
+     */
     public Clause(int n)
     {
         variables = new ArrayList(n); 
     }
-    
+
     /**
      * Evaluate this clause.
-     * @return true only if this clause is true.
+     * @param b An ArrayList containing boolean values.
+     * @return true only if this clause evaluates to true.
      */
     public boolean satisfies(ArrayList<Boolean> b)
     {
+        //since a clause is a disjunction, if one variable is
+        //true, the whole clause is true.
         for(Variable v: variables)
         {  
-            if(v.getValue(b.get(Math.abs(v.getNumber())-1)))
+            boolean varBoo = b.get(v.getNumber()-1); //get boolean 
+            //for corresponding variable.
+            if(v.getValue(varBoo))
                 return true;
         }
         return false;
